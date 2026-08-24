ba3fcb9a89a330c37366a2886af5049b24717eee
juniorchibuike44-sys <juniorchibuike44@gmail.com>
2026-08-24 13:31:36 +0100

    Update pay.js

--- api/pay.js
+++ api/pay.js
@@ -26,7 +26,8 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-
+console.log("VTPASS STATUS:", response.status);
+console.log("VTPASS RESPONSE:", JSON.stringify(data)); 
     return res.status(response.status).json(data);
   } catch (error) {
     return res.status(500).json({
