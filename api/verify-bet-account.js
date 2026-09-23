331c2a68c29647515099185340e29e685dae8a25
juniorchibuike44-sys <juniorchibuike44@gmail.com>
2026-09-23 17:18:09 +0100

    Update verify-bet-account.js

--- api/verify-bet-account.js
+++ api/verify-bet-account.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
     // Pairgate TEST verification endpoint
     const response = await fetch(
-      https://pairgate.com/api/v1/bet/verify 
+      "https://pairgate.com/api/v1/bet/verify", 
       {
         method: "POST",
         headers: {
